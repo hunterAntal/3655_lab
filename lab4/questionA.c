ebc4e0d0bd18c815a71dace0306fd771aaafde5a
hunterAntal <hunterantalgithub@gmail.com>
2024-11-15 14:33:38 -0500

    creating loadPages()

--- lab4/questionA.c
+++ lab4/questionA.c
@@ -3,7 +3,32 @@
 #include <string.h>
 
 int main () {
-    char *str = "Hello, World!";
-    printf("%s\n", str);
+    // this is a simple program that simulates the memory management of a computer system
+    // this is an idealized version of the memory management system of a computer where there are enough frames for each page
+
+    // Secoundary storage each element represents a page in the main storage
+    int storage[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    // program 1 (word) uses 3 pages
+    int program1[3] = {1, 2, 3};
+
+    // program 2 (excel) uses 4 pages
+    int program2[4] = {4, 5, 6, 7};
+
+    // program 3 (power point) uses 3 pages
+    int program3[3] = {8, 9, 10};
+
+    // Main memory containing 10 frames
+    int memory[10] = {0};
+
+
+    
+
+
     return 0;
+}
+
+// function to load a program into memory
+void loadPages(int program[], int storage[], int memory[]){
+    // calculate size of program in pages
 }
