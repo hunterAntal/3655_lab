0b38919827c855e7a19e0b50a06b04e30f868aa2
hunterAntal <hunterantalgithub@gmail.com>
2024-11-16 18:42:06 -0500

    trying to solve segment fault

--- lab4/questionB.c
+++ lab4/questionB.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 10
 typedef struct node
 {
     int val;
@@ -25,151 +24,147 @@ void print_list(node_t *head)
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
 
-// add to the end of the list
 void add(node_t *head, int val)
 {
+    if (head == NULL)
+    {
+        perror("Head is NULL in add()");
+        exit(EXIT_FAILURE);
+    }
+
     node_t *current = head;
     while (current->next != NULL)
     {
         current = current->next;
     }
+
     current->next = (node_t *)malloc(sizeof(node_t));
+    if (current->next == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
     current->next->val = val;
     current->next->next = NULL;
 }
 
-// push to the beggining of the list
 void push(node_t **head, int val)
 {
     node_t *newHead = (node_t *)malloc(sizeof(node_t));
+    if (newHead == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
     newHead->next = *head;
     newHead->val = val;
 
     *head = newHead;
 }
 
-// pop the head of the list aka LRU
 int pop(node_t **head)
 {
-    int newVal;
-    node_t *newHead = (node_t *)malloc(sizeof(node_t));
-    if ((*head)->next == NULL)
+    if (*head == NULL)
+    {
+        printf("List is empty, nothing to pop.\n");
         return -1;
+    }
 
-    newHead = (*head)->next;
-    newVal = (*head)->val;
-    free(*head);
-    *head = newHead;
+    int val = (*head)->val;
+    node_t *temp = *head;
+    *head = (*head)->next;
+    free(temp);
 
-    return newVal;
+    return val;
 }
 
-// search will return 1 if hit and 0 if page fault
 int search(node_t *head, int val)
 {
-    node_t *current = (node_t *)malloc(sizeof(node_t));
+    node_t *current = head;
 
-    current = head;
-
-    while (current->next != NULL)
+    while (current != NULL)
     {
-        // if there is a hit return 1
         if (current->val == val)
         {
-            return 1;
+            return 1; // Hit
         }
-        // move to next node
         current = current->next;
-
-        // if there is a hit return 1
-        if (current->val == val)
-        {
-            return 1;
-        }
     }
-    // page fault return 0
-    return 0;
-}
 
+    return 0; // Page fault
+}
 
 int main()
 {
-
     int hits = 0;
     int pageFaults = 0;
     int frameCount = 0;
     int pageReference = 0;
     float hitRatio = 0.0;
 
-    //int numberOfFrames = 3;
-    //int pageSeq[MAX] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3};
-
-    node_t *head = NULL;
-    head = (node_t *)malloc(sizeof(node_t));
-    head->val = -1;
-    head->next = NULL;
-
-    // Take user input for number of frames and page sequence
-    printf("How many frames would you like to use?:");
+    printf("How many frames would you like to use?: ");
     int numberOfFrames = 0;
     scanf("%d", &numberOfFrames);
-    char input[1000]; // Buffer for user input
-    int *arr = NULL;   // Dynamic array to store numbers
-    int n = 0;         // Number of elements
 
-    printf("Enter numbers separated by spaces: ");
-    fgets(input, sizeof(input), stdin); // Read entire line of input
+    printf("Enter page sequence separated by spaces (end with newline): ");
+    char input[1000];
+    int *arr = NULL;
+    int n = 0;
 
-    // Tokenize the string and convert to integers
+    getchar(); // Clear newline left by scanf
+    if (fgets(input, sizeof(input), stdin) == NULL)
+    {
+        perror("Failed to read input");
+        exit(EXIT_FAILURE);
+    }
+
     char *token = strtok(input, " ");
     while (token != NULL)
     {
-        arr = realloc(arr, (n + 1) * sizeof(int)); // Resize array
+        arr = realloc(arr, (n + 1) * sizeof(int));
         if (arr == NULL)
         {
             perror("Memory allocation failed");
             exit(EXIT_FAILURE);
         }
-        arr[n++] = atoi(token); // Convert string to integer
+
+        arr[n++] = atoi(token);
         token = strtok(NULL, " ");
     }
 
-    printf("You entered: ");
-    for (int i = 0; i < n; i++)
+    // Initialize the linked list
+    node_t *head = (node_t *)malloc(sizeof(node_t));
+    if (head == NULL)
     {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    // Convert dynamic array to static array
-    int pageSeq[n];
-    for (int i = 0; i < n; i++)
-    {
-        pageSeq[i] = arr[i];
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Loop through the page sequence
-    for (int i = 0; i < (sizeof(pageSeq) / sizeof(pageSeq[0])); i++)
+    head->val = -1;
+    head->next = NULL;
+
+    // Simulate page replacement
+    for (int i = 0; i < n; i++)
     {
-        // if page hit then increment hits
         pageReference++;
-        if (search(head, pageSeq[i]) == 1)
+        if (search(head, arr[i]))
         {
             hits++;
         }
         else
         {
-            // page fault so we must check to see if there us a spare frame
             pageFaults++;
             if (frameCount < numberOfFrames)
             {
-                add(head, pageSeq[i]);
+                add(head, arr[i]);
                 frameCount++;
             }
-            else // if there is no free frames remove LRU frame then use it
+            else
             {
                 pop(&head);
-                add(head, pageSeq[i]);
+                add(head, arr[i]);
             }
         }
     }
@@ -178,4 +173,8 @@ int main()
     printf("Number of hits: %d\n", hits);
     printf("Number of page faults: %d\n", pageFaults);
     printf("Hit ratio: %.2f\n", hitRatio);
+
+    free(arr);
+
+    return 0;
 }
